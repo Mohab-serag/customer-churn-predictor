fa1d99122851810ee8d7eb0cc97a6aee6ee3b5ac
Mohab-serag <mohabkhattab2006@gmail,com>
2026-03-08 22:42:46 +0200

    fix model path again

--- app/app.py
+++ app/app.py
@@ -4,8 +4,8 @@ import numpy as np
 import joblib
 
 # ===== Load Model =====
-model = joblib.load("model/churn_model.pkl")
-scaler = joblib.load("model/scaler.pkl")
+model = joblib.load("churn_model.pkl")
+scaler = joblib.load("scaler.pkl")
 
 # ===== Page Config =====
 st.set_page_config(page_title="Customer Churn Predictor", page_icon="📊", layout="wide")
